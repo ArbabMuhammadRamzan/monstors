699d026e992610ecc4c2987259182ba6c152eafa
ArbabMuhammadRamzan <hello.ramzandeveloper@gmail.com>
2020-03-26 20:37:07 +0500

    Updates

---
+++ precache-manifest.93395f7e37bc87183e77607a387b7a9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92aff842ce530c414414fef1de8fff4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a003777d31d779afba27",
+    "url": "/static/css/main.37f94f55.chunk.css"
+  },
+  {
+    "revision": "35413cc94f240e27c663",
+    "url": "/static/js/2.0999fa64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0999fa64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a003777d31d779afba27",
+    "url": "/static/js/main.a933f703.chunk.js"
+  },
+  {
+    "revision": "c9db1fb11c0f0ef53e46",
+    "url": "/static/js/runtime-main.deb88b70.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
